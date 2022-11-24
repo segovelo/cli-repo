b7ac8edeaa614eb026684f5228dca2244e07624c
segovelo <gauchosinpingo@gmail.com>
2022-11-24 23:06:00 +0000

    Changes to send get/post request

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-
 Dependencies:
 pip install PyGithub
 pip install python-dotenv
+pip install requests
--- gitapi.py
+++ gitapi.py
@@ -1,15 +1,38 @@
-from github import Github
+import json
+import requests
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
 access_token = os.getenv("ACCESS_TOKEN")
-# using username and password
-#g = Github("user", "password")
+url = "https://i4vvd0ialb.execute-api.us-east-1.amazonaws.com/Prod/hello"
+urlFakeData = "https://61004cc6bca46600171cf84a.mockapi.io/api-crud/v1/fakeData"
 
-# or using an access token
-g = Github(access_token)
-for repo in g.get_user().get_repos():
-    print(repo.name)
-    repo.edit(has_wiki=False)
+headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+}
+
+data = {
+    "firstName": "Edinson",
+    "lastName": "Cavani",
+    "checkbox": "false"
+}
+
+# To send data form-encoded data
+#response = requests.post(urlFakeData, headers=headers, data=data)
+
+# To send data json-encoded
+response = requests.post(urlFakeData, headers=headers, json=data)
+print("Headers: ", response.headers)
+print("Status Code: ", response.status_code)
+print("Body: ", response.json())
+
+response = requests.get(urlFakeData, headers=headers)  # , json=data)
+print("Headers: ", response.headers)
+print("Status Code: ", response.status_code)
+print("Body: ", response.json())
+# Save response as JSON file
+with open('output.json', 'w') as f:
+    json.dump(response.json(), f)
