e3ea3c306cff039039c2e622b0f9f8a67467f22d
segovelo <gauchosinpingo@gmail.com>
2022-12-11 10:55:44 +0000

    Change to clone, commit and push repo to remote

--- gitapi.py
+++ gitapi.py
@@ -5,6 +5,23 @@ from dotenv import load_dotenv
 import os, sys
 import git
 
+def check_file(file_name, ext):
+    file_name = file_name.translate({ord(i): None for i in '!#@{}[]<>=+£$%^&*()?|,;:/\\\'\"'})
+    index = file_name.find(".")
+    if index > 0:
+        file_name = file_name[0:index]
+    elif index == 0:
+        file_name = file_name[1:]
+    file_name = ".".join([file_name,ext])   
+    return file_name
+
+
+def save(data, save_file, ext):
+    save_file = check_file(save_file, ext)
+    with open(save_file, 'w') as f:
+        f.write(data)
+        f.close()
+
 load_dotenv()
 
 repo_name = sys.argv[1]
@@ -30,14 +47,27 @@ print("Status Code: ", response.status_code)
 print("Response Json : ", response.json)
 """
 
-current_dir = os.getcwd()
+current_dir = os.getcwd().replace("/","\\")
 # parent directory
 parent_dir = os.path.dirname(current_dir)
 
-git.Git(current_dir).clone("https://github.com/{}/{}.git".format(username, repo_name))
+git.Git(parent_dir).clone("https://github.com/{}/{}.git".format(username, repo_name))
 
 os.chdir(os.path.join(parent_dir, repo_name))
 print(os.getcwd())
+
+readme_data = """
+This a README.md test file 
+"""
+save(readme_data, "README", "md")
+
+repo = git.Repo("test-repo")
+repo.git.add('--all')
+repo.git.commit('-m', 'commit message from python script', author='test_user@test.com')
+origin = repo.remote(name="origin")
+origin.push()
+
+
 data1 = {
     "firstName": "Edinson",
     "lastName": "Cavani",
