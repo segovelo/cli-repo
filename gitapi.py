ba8b3d44e4da7132ef12a9237266ae371d3541de
segovelo <gauchosinpingo@gmail.com>
2022-12-22 23:09:40 +0000

    Changes to create repo remotelly and push README.md

--- gitapi.py
+++ gitapi.py
@@ -2,17 +2,20 @@ import json
 import requests
 from requests.auth import HTTPBasicAuth
 from dotenv import load_dotenv
-import os, sys
+import os
+import sys
 import git
 
+
 def check_file(file_name, ext):
-    file_name = file_name.translate({ord(i): None for i in '!#@{}[]<>=+£$%^&*()?|,;:/\\\'\"'})
+    file_name = file_name.translate(
+        {ord(i): None for i in '!#@{}[]<>=+£$%^&*()?|,;:/\\\'\"'})
     index = file_name.find(".")
     if index > 0:
         file_name = file_name[0:index]
     elif index == 0:
         file_name = file_name[1:]
-    file_name = ".".join([file_name,ext])   
+    file_name = ".".join([file_name, ext])
     return file_name
 
 
@@ -22,38 +25,38 @@ def save(data, save_file, ext):
         f.write(data)
         f.close()
 
+
 load_dotenv()
 
 repo_name = sys.argv[1]
 access_token = os.getenv("ACCESS_TOKEN")
 username = os.getenv("USER_NAME")
-url = os.getenv("URL_HELLO")
-urlFakeData = os.getenv("URL_FAKE_DATA")
+
 gitUrl = "https://api.github.com/user/repos"
 headers = {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
 }
 data = {
-    "name": "test-repo",
+    "name": repo_name,
     "private": "false"
 }
-""""
+
 #response = requests.get(gitUrl, auth=HTTPBasicAuth(username, access_token))
 response = requests.post(gitUrl, auth=HTTPBasicAuth(
     username, access_token), headers=headers, json=data)
 print("Headers: ", response.headers)
 print("Status Code: ", response.status_code)
 print("Response Json : ", response.json)
-"""
 
-current_dir = os.getcwd().replace("/","\\")
+current_dir = os.getcwd().replace("/", "\\")
 # parent directory
 parent_dir = os.path.dirname(current_dir)
 
-git.Git(parent_dir).clone("https://github.com/{}/{}.git".format(username, repo_name))
+git.Git(current_dir).clone(
+    "https://github.com/{}/{}.git".format(username, repo_name))
 
-os.chdir(os.path.join(parent_dir, repo_name))
+os.chdir(os.path.join(current_dir, repo_name))
 print(os.getcwd())
 
 readme_data = """
@@ -63,33 +66,8 @@ save(readme_data, "README", "md")
 
 # Changing the CWD
 os.chdir("../")
-repo = git.Repo("test-repo")
+repo = git.Repo(repo_name)
 repo.git.add('--all')
 repo.git.commit("-m", "commit message from python script")
 origin = repo.remote(name="origin")
 origin.push()
-
-
-data1 = {
-    "firstName": "Edinson",
-    "lastName": "Cavani",
-    "checkbox": "false"
-}
-
-# To send data form-encoded data
-#response = requests.post(urlFakeData, headers=headers, data=data)
-""""
-# To send data json-encoded
-response = requests.post(urlFakeData, headers=headers, json=data)
-print("Headers: ", response.headers)
-print("Status Code: ", response.status_code)
-print("Body: ", response.json())
-
-response = requests.get(urlFakeData, headers=headers)  # , json=data)
-print("Headers: ", response.headers)
-print("Status Code: ", response.status_code)
-print("Body: ", response.json())
-# Save response as JSON file
-with open('output.json', 'w') as f:
-    json.dump(response.json(), f)
-"""
--- github-api.py
+++ github-api.py
@@ -1,6 +1,8 @@
 from github import Github
 from dotenv import load_dotenv
 import os
+import json
+import requests
 
 load_dotenv()
 
@@ -13,3 +15,31 @@ g = Github(access_token)
 for repo in g.get_user().get_repos():
     print(repo.name)
     repo.edit(has_wiki=False)
+
+""""
+# To send data form-encoded data
+response = requests.post(urlFakeData, headers=headers, data=data)
+
+url = os.getenv("URL_HELLO")
+urlFakeData = os.getenv("URL_FAKE_DATA")
+
+data = {
+    "firstName": "Edinson",
+    "lastName": "Cavani",
+    "checkbox": "false"
+}
+
+# To send data json-encoded
+response = requests.post(urlFakeData, headers=headers, json=data)
+print("Headers: ", response.headers)
+print("Status Code: ", response.status_code)
+print("Body: ", response.json())
+
+response = requests.get(urlFakeData, headers=headers)  # , json=data)
+print("Headers: ", response.headers)
+print("Status Code: ", response.status_code)
+print("Body: ", response.json())
+# Save response as JSON file
+with open('output.json', 'w') as f:
+    json.dump(response.json(), f)
+"""
